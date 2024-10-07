7df440f429a683d8a0265a45f589d96092bc0af0
= <=>
2024-10-07 17:46:16 +0300

    n

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-// import * as ReactDOM from "react-dom";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
