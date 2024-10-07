a5e17abf02f015af2b737cfe6fe35a530f43aa09
= <=>
2024-10-07 17:42:12 +0300

    add accesbility

--- src/components/NavList/NavList.tsx
+++ src/components/NavList/NavList.tsx
@@ -2,7 +2,6 @@ import { NavLink } from "react-router-dom";
 import CheckvinLogo from "../Icons/CheckvinLogo";
 import NavItem from "../NavItem/NavItem";
 import { NavItem as INavItem } from "../../types";
-import { Fragment } from "react/jsx-runtime";
 import "./NavList.scss";
 
 interface NavListProps {
@@ -17,14 +16,18 @@ const NavList: React.FC<NavListProps> = ({ items }) => {
       <ul className="nav-list__container">
         {items.map((item, i) => {
           return (
-            <Fragment key={i}>
-              {i === mid && (
-                <NavLink to="/" className="nav-item nav-list__item nav-item--logo">
-                  <CheckvinLogo />
-                </NavLink>
-              )}
-              <NavItem link={item.link} name={item.name} />
-            </Fragment>
+            <>
+              <li className="nav-item nav-list__item nav-item--logo">
+                {i === mid && (
+                  <NavLink to="/" className="">
+                    <CheckvinLogo />
+                  </NavLink>
+                )}
+              </li>
+              <li key={i} className="nav-item nav-list__item">
+                <NavItem link={item.link} name={item.name} />
+              </li>
+            </>
           );
         })}
       </ul>
